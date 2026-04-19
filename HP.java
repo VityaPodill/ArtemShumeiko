af41e16bd4b088856b4a3aa19fc7c32dd40fe79b
VityaPodill <119930971+VityaPodill@users.noreply.github.com>
2026-04-19 21:28:26 +0300

    Squash and merge (TTL)
    * Squash and merge_1
    
    * Squash and merge_2
    
    * Squash and merge_3
    
    ---------
    
    Co-authored-by: Viktor Churylo <Viktor.Churylo@firstgroup.com>

--- HP.java
+++ HP.java
@@ -1,12 +1,8 @@
 public class HP {
     public static void main(String[] args) {
-        String _1 = "Squash and merge";
-        System.out.println(_1);
-
-        String _2 = "";
-        System.out.println(_2);
-
-        int _3 = 333;
-        System.out.println(_3);
+        double Squash_and_merge = 1;
+        int _2 = 222;
+        String _3 = "www";
+        System.out.println(Squash_and_merge + _2 + _3);
     }
 }
