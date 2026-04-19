dec1aa8076af90b2a3ab9a7bc9d8bd115883d257
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2026-04-19 21:16:18 +0300

    I forgot to create a new branch :(((

---
+++ HP.java
@@ -0,0 +1,12 @@
+public class HP {
+    public static void main(String[] args) {
+        String _1 = "Squash and merge";
+        System.out.println(_1);
+
+        String _2 = "";
+        System.out.println(_2);
+
+        int _3 = 333;
+        System.out.println(_3);
+    }
+}
