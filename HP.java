cf25aa3aa3bf599e3b72c395e1ffb0bb3f60f84e
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2026-04-19 21:24:21 +0300

    Squash and merge_3

--- HP.java
+++ HP.java
@@ -1,8 +1,8 @@
 public class HP {
     public static void main(String[] args) {
         double Squash_and_merge = 1;
-
         int _2 = 222;
-        System.out.println(Squash_and_merge + _2);
+        String _3 = "www";
+        System.out.println(Squash_and_merge + _2 + _3);
     }
 }
