a64c07ad9d6fc82b0c75f923e9ec21289dd15a49
ACER_Laptop <ACER@gmail.com>
2026-04-20 14:18:03 +0300

    ACER_5_5_5

--- ACER_REBASE_Class.java
+++ ACER_REBASE_Class.java
@@ -1,13 +1,14 @@
 public class ACER_REBASE_Class {
     public static void main(String[] args) {
-        String a="ACER_REBASE_";
+        String a = "ACER_REBASE_";
         System.out.println(a);
 
-        int rebase=111;
-        System.out.println(rebase-11);
-        int d=-1;
-
+        int rebase = 111;
+        System.out.println(rebase - 11);
         String aaa = "new REBASE_2";
-        System.out.println(aaa);
+
+        int d1 = 1234567890;
+        long d2 = 10000L;
+        System.out.println(aaa + d2 + d1);
     }
 }
