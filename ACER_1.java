26ecca09e22d0f9a0392c9d5e3d7f626a5d90c5a
qa-tester-eng <churyloviktor@gmail.com>
2026-04-20 12:50:07 +0300

    Acer 4444 (#17)
    * ACER_4444

--- ACER_1.java
+++ ACER_1.java
@@ -4,6 +4,8 @@ public class ACER_1 {
         int ACER_2 = 222;
 
         int ACER_3 = 333;
-        System.out.println(ACER_11 + ACER_2 + ACER_3);
+
+        int ACER_44__44 = 44444;
+        System.out.println(ACER_11 + ACER_2 + ACER_3 + ACER_44__44);
     }
 }
