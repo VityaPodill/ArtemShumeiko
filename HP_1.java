41bbb002eec83d8a0899bae9843737a37bf3e8ea
Viktor Churylo <Viktor.Churylo@firstgroup.com>
2026-04-20 11:54:38 +0300

    HP_3

--- HP_1.java
+++ HP_1.java
@@ -2,6 +2,8 @@ public class HP_1 {
     public static void main(String[] args) {
         String HP_11 = "";
         int HP_2 = 222;
-        System.out.println(HP_11 + HP_2);
+
+        int HP_3 = 333;
+        System.out.println(HP_11 + HP_2 + HP_3);
     }
 }
