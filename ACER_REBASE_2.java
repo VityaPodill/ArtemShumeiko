bbae7e39c37ecc5d61040daa377071079415c7cb
ACER_Laptop <ACER@gmail.com>
2026-04-20 20:54:29 +0300

    from ACER 2

--- ACER_REBASE_2.java
+++ ACER_REBASE_2.java
@@ -1,6 +1,7 @@
 public class ACER_REBASE_2 {
     public static void main(String[] args) {
         String a = "ACER_REBASE_2";
-        System.out.println(a);
+        int b = 2;
+        System.out.println(a + b);
     }
 }
