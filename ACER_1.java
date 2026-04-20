c4ba5b53b13c07363ac45effbb581a546408fc66
ACER_Laptop <ACER@gmail.com>
2026-04-20 12:07:16 +0300

    ACER_333

--- ACER_1.java
+++ ACER_1.java
@@ -2,6 +2,8 @@ public class ACER_1 {
     public static void main(String[] args) {
         int ACER_11 = 1;
         int ACER_2 = 222;
-        System.out.println(ACER_11 + ACER_2);
+
+        int ACER_3 = 333;
+        System.out.println(ACER_11 + ACER_2 + ACER_3);
     }
 }
