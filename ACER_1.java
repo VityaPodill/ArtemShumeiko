55224a55c4d6adc0892a9183c1d81f8f4404107c
ACER_Laptop <ACER@gmail.com>
2026-04-20 12:45:14 +0300

    ACER_4444

--- ACER_1.java
+++ ACER_1.java
@@ -4,6 +4,8 @@ public class ACER_1 {
         int ACER_2 = 222;
 
         int ACER_3 = 333;
-        System.out.println(ACER_11 + ACER_2 + ACER_3);
+
+        int ACER_4444 = 4444;
+        System.out.println(ACER_11 + ACER_2 + ACER_3 + ACER_4444);
     }
 }
