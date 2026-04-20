2289b1f78083e8d782e28119f76672c5f9527281
ACER_Laptop <ACER@gmail.com>
2026-04-20 14:18:10 +0300

    Revert "Acer 4444 (#17)"
    This reverts commit 26ecca09e22d0f9a0392c9d5e3d7f626a5d90c5a.

--- ACER_1.java
+++ ACER_1.java
@@ -4,8 +4,6 @@ public class ACER_1 {
         int ACER_2 = 222;
 
         int ACER_3 = 333;
-
-        int ACER_44__44 = 44444;
-        System.out.println(ACER_11 + ACER_2 + ACER_3 + ACER_44__44);
+        System.out.println(ACER_11 + ACER_2 + ACER_3);
     }
 }
